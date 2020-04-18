17c3c195ea2f31f24559dc1fd72943b0c679ab2f
DangerBlack <danger.recheng@hotmail.it>
2020-04-19 00:01:28 +0200

    feat(EnemyAi): add Enemy flying

---
+++ Assets/Scripts/Colliders/DotCollider.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DotCollider : MonoBehaviour
+{
+    public bool collide = false;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        collide = true;
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        collide = false;
+    }
+
+    public bool IsCollide()
+    {
+        return this.collide;
+    }
+}
