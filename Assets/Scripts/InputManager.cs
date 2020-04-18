90d344b1a6476749e9c049f802861103a6f71129
Federico 'Morg' Pareschi <morgawr@gmail.com>
2020-04-18 12:40:52 +0900

    Fix PS4 mappings/controller setup

--- Assets/Scripts/InputManager.cs
+++ Assets/Scripts/InputManager.cs
@@ -3,19 +3,22 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public static class InputManager {
+
     public static bool IsPressed(string action) {
 
+        var horizAxis = Input.GetAxis("Horizontal");
+        var vertAxis = Input.GetAxis("Vertical");
         switch(action.ToLower()) {
             case "left":
-                return Input.GetKey("a") || Input.GetKey("left");
+                return Input.GetKey("a") || Input.GetKey("left") || horizAxis < -0.2;
             case "right":
-                return Input.GetKey("d") || Input.GetKey("right");
+                return Input.GetKey("d") || Input.GetKey("right") || horizAxis > 0.2;
             case "up":
-                return Input.GetKey("w") || Input.GetKey("up");
+                return Input.GetKey("w") || Input.GetKey("up") || vertAxis > 0.2;
             case "down":
-                return Input.GetKey("s") || Input.GetKey("down");
+                return Input.GetKey("s") || Input.GetKey("down") || vertAxis < -0.2;
             case "jump":
-                return Input.GetKey("space");
+                return Input.GetKey("space") || Input.GetButton("Jump");
         }
         return false;
     }
