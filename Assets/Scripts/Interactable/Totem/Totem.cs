0573c3ff8a5872a3a627e1c21ebbc1abb6197c65
DangerBlack <danger.recheng@hotmail.it>
2020-04-18 20:34:03 +0200

    feat(Totem): add totem

---
+++ Assets/Scripts/Interactable/Totem/Totem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Totem : Interactable
+{
+    public string message;
+
+    public override void Interact()
+    {
+        // TODO: Implement interaction with portal
+        Debug.Log("Trigger Totem interaction");
+        //GameObject.findWithTag("Respawn");
+
+    }
+}
