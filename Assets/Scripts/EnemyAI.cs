a7cd9d653900e298f47d677972449a4e1cbe4f91
Federico 'Morg' Pareschi <morgawr@gmail.com>
2020-04-18 15:17:59 +0900

    Add basic enemy layout and AI structure

---
+++ Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+
+    public ControllableComponent Player;
+
+    public delegate void AttackRoutine();
+
+
+    public AttackRoutine[] RangedAttacks;
+    public AttackRoutine[] MeleeAttacks;
+
+    public float MeleeRange = 0;
+    public float AttackSpeed = 0;
+    bool hasJustAttacked = false;
+    bool isPlayerSpotted = false;
+
+    Timer attackTimer = new Timer();
+
+    void Start() {
+        
+    }
+
+    void resetAttackCooldown() {
+        hasJustAttacked = false;
+    }
+
+    float CalculateDistanceFromPlayer() {
+        return Physics2D.Distance(Player.GetComponent<Collider2D>(), this.GetComponent<Collider2D>()).distance;
+    }
+
+    protected virtual void Patrol() {
+        // Patrol routine
+        Debug.Log("patrolling");
+    }
+
+    protected virtual void MeleeAttack() {
+        // Melee attack
+        Debug.Log("melee attack");
+    }
+    
+    protected virtual void RangedAttack() {
+        // Ranged attack
+        Debug.Log("ranged attack");
+    }
+
+    protected virtual void ApproachPlayer() {
+        Debug.Log("Approaching player");
+    }
+
+    public virtual void PlayerSpotted(bool spotted) {
+        isPlayerSpotted = spotted;
+    }
+
+    void Update() {
+        if(!isPlayerSpotted) {
+            Patrol();
+        } else if(!hasJustAttacked) {
+            if(CalculateDistanceFromPlayer() > MeleeRange) {
+                RangedAttack();
+            } else {
+                MeleeAttack();
+            }
+            hasJustAttacked = true;
+            StartCoroutine(attackTimer.Countdown(1f / AttackSpeed, new Timer.SideEffector(resetAttackCooldown)));
+        } else {
+            // TODO: Add different logic for melee vs non-melee enemy types
+            ApproachPlayer();
+        }
+    }
+
+    void OnDrawGizmosSelected () {
+        Gizmos.color = new Color(1.0f, 0f, 0f, .2f);
+        Gizmos.DrawSphere(transform.position, MeleeRange);
+    }
+}
---
+++ Assets/Scripts/PlayerDetectorComponent.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerDetectorComponent : MonoBehaviour
+{
+
+    public ControllableComponent player;
+    public EnemyAI ai;
+
+    public bool HasLOS = true;
+    public Transform LOSOrigin;
+
+    Collider2D selfCollider = null;
+
+    void Start() {
+        selfCollider = this.GetComponent<Collider2D>();
+    }
+
+    void OnTriggerStay2D(Collider2D other) {
+        if (selfCollider.IsTouchingLayers(LayerMask.GetMask("PlayerLayer"))) {
+            if (!HasLOS) {
+                ai.PlayerSpotted(true);
+                return;
+            }
+            var losLayerMask = LayerMask.GetMask("PlayerLayer", "PlatformLayer");
+            var direction = (player.transform.position - LOSOrigin.transform.position).normalized;
+            RaycastHit2D sightTest = Physics2D.Raycast(LOSOrigin.transform.position, direction, Mathf.Infinity, losLayerMask);
+            if (sightTest.collider != null) {
+                if(sightTest.collider.gameObject == player.gameObject) {
+                    ai.PlayerSpotted(true);
+                    return;
+                }
+            }
+            ai.PlayerSpotted(false);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other) {
+        if (!selfCollider.IsTouchingLayers(LayerMask.GetMask("PlayerLayer"))) {
+            ai.PlayerSpotted(false);
+        }
+    }
+}
