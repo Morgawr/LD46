e38036cf01a1b239d7a524c557ffa5ede489d326
Federico 'Morg' Pareschi <morgawr@gmail.com>
2020-04-18 13:18:10 +0900

    Add ladder movement and air strafing

--- Assets/Scripts/ControllableComponent.cs
+++ Assets/Scripts/ControllableComponent.cs
@@ -6,11 +6,14 @@ public class ControllableComponent : MonoBehaviour
 {
 
     public Rigidbody2D body = null;
+    public Collider2D avatarCollider = null;
 
     public Player Player;
 
     bool isInAir = false;
     bool isJumpCooldown = false;
+
+    bool isOnLadder = false;
 
     Timer jumpTimer;
 
@@ -19,11 +22,21 @@ public class ControllableComponent : MonoBehaviour
     }
 
     public bool CanMove() {
-        return !isInAir;
+        return !isInAir || isOnLadder;
     }
 
     public void SignalInAir(bool start){
         isInAir = start;
+    }
+
+    public void SignalIsClimbing(bool start) {
+        if(start) {
+            body.gravityScale = 0;
+            body.velocity = new Vector2(0, 0);
+        } else {
+            body.gravityScale = 1;
+        }
+        isOnLadder = start;
     }
 
     // Start is called before the first frame update
@@ -38,16 +51,36 @@ public class ControllableComponent : MonoBehaviour
 
     // Update is called once per frame
     void Update() {
-        if(InputManager.IsPressed("left") && CanMove()) {
-            body.AddForce(new Vector2(-1, 0) * Player.moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
-        }
-        else if (InputManager.IsPressed("right") && CanMove())
-            body.AddForce(new Vector2(1, 0) * Player.moveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+
+        var actualMoveSpeed = isInAir ? Player.airSpeed : Player.moveSpeed;
+
+        if(InputManager.IsPressed("left") ) //&& CanMove()) 
+            body.AddForce(new Vector2(-1, 0) * actualMoveSpeed * Time.deltaTime, ForceMode2D.Impulse);
+        else if (InputManager.IsPressed("right")) // && CanMove())
+            body.AddForce(new Vector2(1, 0) * actualMoveSpeed * Time.deltaTime, ForceMode2D.Impulse);
 
         if(InputManager.IsPressed("jump") && CanMove() && !isJumpCooldown) {
             body.AddForce(new Vector2(0, 1) * Player.jumpStrength);
             isJumpCooldown = true;
+            this.SignalIsClimbing(false);
             StartCoroutine(jumpTimer.Countdown(0.5f, new Timer.SideEffector(resetJumpCooldown)));
+        }
+
+        if (InputManager.IsPressed("up") || InputManager.IsPressed("down")) {
+            if(avatarCollider.IsTouchingLayers(LayerMask.GetMask("LadderLayer"))) {
+                this.SignalIsClimbing(true);
+            }
+        }
+
+        if(isOnLadder) {
+            var climbForce = new Vector2(0, 0);
+            if(InputManager.IsPressed("up")) {
+                climbForce = new Vector2(0, 1);
+            }
+            if(InputManager.IsPressed("down")) {
+                climbForce = new Vector2(0, -1);
+            }
+            transform.Translate(climbForce * Player.climbSpeed * Time.deltaTime);
         }
 
         ClampVelocity();
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 0;
+    public float airSpeed = 0;
     public float maxVelocity = 0;
     public float jumpStrength = 0;
+    public float climbSpeed = 0;
 }
