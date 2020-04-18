440041ffbc49ca68e535a6801411e06cc46d0d13
DangerBlack <danger.recheng@hotmail.it>
2020-04-18 17:24:29 +0200

    feat(Checkpoint): add checkpoint

---
+++ Assets/Scripts/Interactable/Checkpoint/Checkpoint.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : Interactable
+{
+    public override void Interact()
+    {
+        // TODO: Implement interaction with portal
+        Debug.Log("Trigger Checkpoint interaction");
+    }
+}
