9e177cadb0196d160662b5e185d43a53f2afc4f3
DangerBlack <danger.recheng@hotmail.it>
2020-04-19 17:45:24 +0200

    feat(tooltip): add tooltip message on hover (#6)

--- Assets/Scripts/Interactable/Interactable.cs
+++ Assets/Scripts/Interactable/Interactable.cs
@@ -6,17 +6,30 @@ public abstract class Interactable : MonoBehaviour
 {
     public ControllableComponent controller;
     public Player Player;
+    public string tooltip = "Press up to interact";
+
+    protected bool isOnTooltip = false;
 
     public abstract void Interact();
 
     void OnTriggerEnter2D(Collider2D other)
     {
         controller.SignalIsOnInteractable(this);
+        isOnTooltip = true;
     }
 
     void OnTriggerExit2D(Collider2D other)
     {
         controller.SignalIsOnInteractable(null);
+        isOnTooltip = false;
+    }
+
+    protected void OnGUI()
+    {
+        if (isOnTooltip)
+        {
+            GUI.Label(new Rect((Screen.width - 150f) / 2, Screen.height / 10, 200f, 200f), tooltip);
+        }
     }
 
     protected virtual void Start() {
