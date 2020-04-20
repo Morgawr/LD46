3cd379604ca17b020acdce43ad80ff26419c78f3
Federico 'Morg' Pareschi <morgawr@gmail.com>
2020-04-20 19:22:13 +0900

    Added last room scene

---
+++ Assets/Scripts/SelfDestructComponent.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestructComponent : MonoBehaviour
+{
+
+    // If you add this to a component it will delete itself on load.
+
+    // Update is called once per frame
+    void Update() {
+        GameObject.Destroy(this.gameObject);
+    }
+}
